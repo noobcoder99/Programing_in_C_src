a83272806cd3173558baf0bebef31583f2dddfd7
asd <jofmc858@gmail.com>
2019-01-08 18:19:10 +1000

    new

---
+++ ex1.c
@@ -0,0 +1,13 @@
+//Excersise in C printing lines
+
+#include<stdio.h>
+int main(void)
+{
+  printf("In C, lowercase letters are significate\n", );
+  printf("main() is where the program execution begins\n", );
+  printf("Opening and closing braces enclose program statmnts in routine\n", );
+  printf("All program statments must be terminated by a semicolon\n", );
+  
+  return 0;
+
+}
---
+++ foo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+  int sum;
+  sum = 50 + 50;
+  printf("The sum is %i\n",sum );
+
+  return 0;
+  
+
+
+}
